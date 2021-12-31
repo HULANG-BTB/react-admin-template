adaf675ee41013d97b1952f66eda23cc28fb417c
胡浪 <admin@oibit.cn>
2021-12-31 16:42:15 +0800

    feat: 登录页面

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom'
+import {Form, Input, Button} from 'antd';
+import './index.scss'
 
 const Login: React.FC = () => {
-  return <div>login</div>
+  const navigate = useNavigate();
+  const onFinish = (values: any) => {
+    const {username, password} = values
+    if (username === password) {
+      navigate('/dashboard')
+    }
+  };
+
+  return <div className='login-page'>
+    <div className='login-box'>
+      <h2 className={'title'}>登录</h2>
+      <div className={'form'}>
+        <Form
+          name="basic"
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="username"
+            rules={[{required: true, message: 'Please input your username!'}]}
+          >
+            <Input className={'login-input'} placeholder={'Username'}/>
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{required: true, message: 'Please input your password!'}]}
+          >
+            <Input.Password className={'login-input'} placeholder={'Password'}/>
+          </Form.Item>
+
+          <Form.Item>
+            <Button className={'login-button'} block type="default" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  </div>
 }
 
 export default Login
